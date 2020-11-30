9e843f4bb43c18bf85d4506fd0d7c4e4037639bf
Thomas Winter <wintert98@gmail.com>
2020-11-30 19:42:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,19 @@ game_hash = {
   :home => {
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
-    :players => []
+    :players => [{
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+    }
+      
+      ]
   },
   :away => {
     :team_name => "Charlotte Hornets",
